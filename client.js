4a249d28f04665141506d5184c84e00098be0c5c
inkerinmaa <techniker357@gmail.com>
2021-07-27 18:24:05 +0000

    mqtt publishing added

--- client.js
+++ client.js
@@ -7,13 +7,10 @@ const { OPCUAClient,
   TimestampsToReturn,
   ClientMonitoredItem,
   ClientMonitoredItemGroup,
-  DataType} = require("node-opcua");
-const { isIndexedAccessTypeNode } = require("typescript");
+  DataType} = require("node-opcua")
 
+const publish = require("./mqtt/publish.js")
 const { seq_auth, updateTags } = require('./models/tag.js')
-
-seq_auth()
-
 const connectionStrategy = {
     initialDelay: 1000,
     maxRetry: 1
@@ -25,22 +22,24 @@ const options = {
     securityMode: MessageSecurityMode.None,
     securityPolicy: SecurityPolicy.None,
     endpointMustExist: false,
-};
-const client = OPCUAClient.create(options);
-// const endpointUrl = "opc.tcp://opcuademo.sterfive.com:26543";
-const endpointUrl = "opc.tcp://" + require("os").hostname() + ":4334/UA/MyLittleServer";
+}
+
+const client = OPCUAClient.create(options)
+const endpointUrl = "opc.tcp://" + require("os").hostname() + ":4334/UA/MyLittleServer"
+
+seq_auth() // Sequelize authenticate to DB
 
 async function main() {
     try {
       // step 1 : connect to
       await client.connect(endpointUrl);
-      console.log("connected !");
+      console.log("connected !")
   
       // step 2 : createSession
-      const session = await client.createSession();
-      console.log("session created !");
+      const session = await client.createSession()
+      console.log("session created !")
   
-      // // // step 3 : browse
+      // step 3 : browse
       // const browseResult = await session.browse("RootFolder");
 
       // console.log("references of RootFolder :");
@@ -53,32 +52,31 @@ async function main() {
       // console.log(" value = " , dataValue2.toString());
   
       // step 4' : read a variable with read
-    //   const maxAge = 0;
-    //   const nodesToRead = {
-    //     names: ['Counter', 'Static', 'FreeMem'],
-    //     nodesToRead: [
-    //     { nodeId: "ns=1;i=1", attributeId: AttributeIds.Value },
-    //     { nodeId: "ns=1;i=2", attributeId: AttributeIds.Value },
-    //     { nodeId: "ns=1;i=3", attributeId: AttributeIds.Value }
-    //   ]
-    // }
-    //   // const nodeToRead = {
-    //   // nodeId: "ns=1;i=2",
-    //   // attributeId: AttributeIds.Value
-    //   // }
+      const maxAge = 0;
+      const nodesToRead = {
+        names: ['Counter', 'Static', 'FreeMem'],
+        nodesToRead: [
+        { nodeId: "ns=1;i=1", attributeId: AttributeIds.Value },
+        { nodeId: "ns=1;i=2", attributeId: AttributeIds.Value },
+        { nodeId: "ns=1;i=3", attributeId: AttributeIds.Value }
+      ]
+    }
+      // const nodeToRead = {
+      // nodeId: "ns=1;i=2",
+      // attributeId: AttributeIds.Value
+      // }
 
     // setInterval(async () => {
 
     //   const dataValue =  await session.read(nodesToRead.nodesToRead, maxAge);
     //   console.log(" value " , dataValue)
 
-
     //   // // Write data to DB
     //   let writeToDB =  function(dataValue) {
     //     for (let k = 0; k < dataValue.length; k++) {
-    //       // console.log(nodesToRead.nodesToRead[k].nodeId)
+    //       console.log(nodesToRead.nodesToRead[k].nodeId)
     //       let dataToDB_Read = {id: (k+1), name: (nodesToRead.names[k]), address: ('ns=' + nodesToRead.nodesToRead[k].nodeId.namespace + ',i=' + nodesToRead.nodesToRead[k].nodeId.value), value: dataValue[k].value.value, qualityText: dataValue[k].statusCode._name}
-    //       // console.log(dataToDB_Read)
+    //       console.log(dataToDB_Read)
     //       updateTags(dataToDB_Read)
     //     }
     //   }
@@ -97,9 +95,10 @@ async function main() {
       //            value: 243
       //       }
       //   }
-      // }];
-      // let writeResult = await session.write(nodesToWrite);
-      //   console.log(writeResult.toString());
+      // }]
+
+      // let writeResult = await session.write(nodesToWrite)
+      //   console.log(writeResult.toString())
   
       // step 5: install a subscription and install a monitored item for 10 seconds
       const subscription = ClientSubscription.create(session, {
@@ -109,15 +108,15 @@ async function main() {
         maxNotificationsPerPublish:  100,
         publishingEnabled: true,
         priority: 10
-    });
+    })
     
     subscription.on("started", function() {
         console.log("subscription started for 2 seconds - subscriptionId=", subscription.subscriptionId);
     }).on("keepalive", function() {
-        console.log("keepalive");
+        console.log("keepalive")
     }).on("terminated", function() {
-       console.log("terminated");
-    });
+       console.log("terminated")
+    })
     
     
     // install monitored item
@@ -136,40 +135,44 @@ async function main() {
         ]
       }
 
-
     const parameters = {
         samplingInterval: 100,
         discardOldest: true,
         queueSize: 10
-    };
+    }
     
     const monitoredItems  = ClientMonitoredItemGroup.create(
         subscription,
         itemsToMonitor.itemsToMonitor,
         parameters,
         TimestampsToReturn.Both
-    );
+    )
     
     monitoredItems.on("changed", (monitoredItem, dataValue, index) => {
       //  console.log(" value has changed : ", dataValue.value.toString())
-      console.log("Changed on ", index ,  dataValue.value.value.toString())
+      let changed = ("Changed on " + index + dataValue.value.value.toString())
+      console.log(changed)
 
-      //  { id: 2, name: 'Static_Upd', address: 'ns=1;i=2', value: 222.90, qualityText: 'Bad' }
+    let dataToDB = {
+      id: (index+1),
+      name: (itemsToMonitor.names[index]),
+      address: ('ns=' + itemsToMonitor.itemsToMonitor[index].nodeId.namespace + ',i=' + itemsToMonitor.itemsToMonitor[index].nodeId.value),
+      value: dataValue.value.value, qualityText: dataValue.statusCode._name}
 
-    let dataToDB = {id: (index+1), name: (itemsToMonitor.names[index]), address: ('ns=' + itemsToMonitor.itemsToMonitor[index].nodeId.namespace + ',i=' + itemsToMonitor.itemsToMonitor[index].nodeId.value), value: dataValue.value.value, qualityText: dataValue.statusCode._name}  
-    // console.log(dataToDB)
     updateTags(dataToDB)
-    });
+    publish(changed)
+
+    })
     
     
     
     async function timeout(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-    await timeout(40000);
+    await timeout(2000)
     
-    console.log("now terminating subscription");
-    await subscription.terminate();
+    console.log("now terminating subscription")
+    await subscription.terminate()
   
       // // step 6: finding the nodeId of a node by Browse name
       // const browsePath = makeBrowsePath("RootFolder", "/Objects/1:MyDevice.1:Static");
@@ -181,16 +184,14 @@ async function main() {
     // console.log(" Product Name nodeId = ", productNameNodeId.toString());
   
       // close session
-      await session.close();
+      await session.close()
   
       // disconnecting
-      await client.disconnect();
-      console.log("done !");
+      await client.disconnect()
+      console.log("done !")
     } catch(err) {
-      console.log("An error has occured : ",err);
+      console.log("An error has occured : ",err)
       }
 }
 
 main()
-
-module.exports = main;
---
+++ mqtt/publish.js
@@ -0,0 +1,27 @@
+const amqp = require('amqplib')
+const dbConfig = require('../models/db.config')
+const config = require('../models/db.config')
+const serverIP = config.host
+
+function publish(dataToMqtt) {
+  amqp.connect('amqp://' + serverIP).then(function(conn) {
+  return conn.createChannel().then(function(ch) {
+    const q = 'hello'
+
+    let ok = ch.assertQueue(q, {durable: false})
+
+    return ok.then(function(_qok) {
+      // NB: `sentToQueue` and `publish` both return a boolean
+      // indicating whether it's OK to send again straight away, or
+      // (when `false`) that you should wait for the event `'drain'`
+      // to fire before writing again. We're just doing the one write,
+      // so we'll ignore it.
+      ch.sendToQueue(q, Buffer.from(dataToMqtt))
+      console.log(" [x] Sent '%s'", dataToMqtt)
+      return ch.close()
+    });
+  }).finally(function() { conn.close() })
+}).catch(console.warn)
+}
+
+module.exports = publish
